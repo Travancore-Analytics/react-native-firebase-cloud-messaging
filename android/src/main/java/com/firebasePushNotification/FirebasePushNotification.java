a722b94280dc27bf1c3a66883c95132d1d438104
Shahulul faris p <shahulul.fp@teamta.in>
2021-03-01 10:01:24 +0530

    FCM token generation

--- android/src/main/java/com/firebasePushNotification/FirebasePushNotification.java
+++ android/src/main/java/com/firebasePushNotification/FirebasePushNotification.java
@@ -2,6 +2,7 @@ package com.firebasePushNotification;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.facebook.react.bridge.Callback;
@@ -11,6 +12,8 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import javax.annotation.Nonnull;
@@ -66,6 +69,25 @@ public class FirebasePushNotification extends ReactContextBaseJavaModule {
     @ReactMethod
     public void checkPermissionForPushNotification(Callback callback) {
         callback.invoke(null,NotificationManagerCompat.from(getCurrentActivity()).areNotificationsEnabled());
+    }
+
+
+    @ReactMethod
+    public void getToken(final Callback callback) {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("exception", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+                        callback.invoke(task.getResult().getToken());
+                    }
+                });
     }
 
     @Override
--- ios/FirebasePushNotification/FirebasePushNotification.m
+++ ios/FirebasePushNotification/FirebasePushNotification.m
@@ -94,6 +94,17 @@ RCT_EXPORT_METHOD(unsubscribeFromTopic: (NSString*) topic
     }];
 }
 
+RCT_EXPORT_METHOD(getToken:(RCTResponseSenderBlock)callback) {
+    NSString *token = [[FIRMessaging messaging] FCMToken];
+    if (token == nil) {
+        NSLog(@"There is no token from firebase");
+    }else {
+        NSLog(@"FCM registration token: %@", token);
+        callback(@[token]);
+    }
+}
+
+
 RCT_EXPORT_METHOD(checkPermissionForPushNotification:(RCTResponseSenderBlock)callback) {
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
     [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings *settings){
